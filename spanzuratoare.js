784b3acf043a345e994a6a472b0b8d4f428244f5
CurtuzanDanut <58391440+CurtuzanDanut@users.noreply.github.com>
2022-04-02 20:51:02 +0300

    Add files via upload

---
+++ spanzuratoare.js
@@ -0,0 +1,64 @@
+let word 
+let answerArray = []
+let remainingLetters
+let checkLetter
+let guess
+let lives = 10
+let usedLettersArray = []
+
+function start(){
+	word = document.getElementById('wordToGuess').value
+	word = word.toUpperCase()
+	document.getElementById('wordToGuess').value = ''
+	for(let i = 0; i < word.length; i++){
+		answerArray[i] = "_"
+	}
+			document.getElementById('displayed-word').innerHTML = answerArray.join(" ")
+	remainingLetters = word.length
+	return false
+}
+
+function tryLetter(){
+	checkLetter = remainingLetters
+	guess = document.getElementById('letter').value
+	guess = guess.toUpperCase()
+	document.getElementById('letter').value = ''
+	usedLetters()
+	if(guess === null){
+		alert("Please enter a letter")
+	}else if(guess.length !== 1){
+		alert("Enter a single letter")
+	}else{
+		for(let j = 0; j < word.length; j++){
+			if(word[j] === guess){
+				answerArray[j] = guess
+				remainingLetters -= 1
+			}
+		}
+	}
+	if((remainingLetters === checkLetter) && lives >= 1){
+		document.getElementById("image").src = "hang" + lives + ".jpg"
+		lives--
+		if(lives === 0){
+			alert("You lost, the word was " + word)
+			restart()
+		}
+	}
+	document.getElementById('displayed-word').innerHTML = answerArray.join(" ")
+	if(remainingLetters === 0){
+		alert("Congratulations, you won!!")
+		restart()
+	}
+	return false
+}
+
+function usedLetters(){
+	usedLettersArray[usedLettersArray.length] = guess
+	for(let x = 0; x < usedLettersArray.length; x++){
+		document.getElementById("usedLetters").innerHTML = usedLettersArray.join(", ")
+	}
+}
+
+function restart(){
+	location.reload()
+}
